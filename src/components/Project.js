ca9c6fcfc44a640d897df14efaea62c22aa66208
Atchuross <57393426+Atchuross@users.noreply.github.com>
2021-05-03 16:32:05 +0200

    Revert "🛠 project section not responve"
    This reverts commit 1b55497823a3a59590048d67157df8aadf64c7fb.

--- src/components/Project.js
+++ src/components/Project.js
@@ -14,27 +14,27 @@ const Project = () => {
         <div className="fake-magicwall" id="home-magicwalle">
             <ul>
                 <li id="litest1">
-                    <section id="tile1" className="magic-wall_item lazyloaded" style={{backgroundImage: `url("https://upload.wikimedia.org/wikipedia/commons/7/78/Swift_logo.png")`}}></section>
+                    <section id="tile1" className="magic-wall_item lazyloaded" style={{backgroundImage: `url("https://jacekjeznach.com/wp-content/uploads/2017/10/Bez-nazwy-3-e1508357920340.jpg")`}}></section>
                     <div className="hover-content vcenter tile1-vcenter"></div>
                     <a href="/" className="colorbox cboxElement"></a>
                 </li>
                 <li id="litest2">
-                    <section className="magic-wall_item lazyloaded" style={{backgroundImage: `url("https://upload.wikimedia.org/wikipedia/commons/6/6a/JavaScript-logo.png")`}}></section>
+                    <section className="magic-wall_item lazyloaded" style={{backgroundImage: `url("https://jacekjeznach.com/wp-content/uploads/2019/11/ttelo.png")`}}></section>
                     <div className="hover-content vcenter"></div>
                     <a href="/" className="colorbox cboxElement"></a>
                 </li>
                 <li id="litest3">
-                    <section className="magic-wall_item lazyloaded" style={{backgroundImage: `url("https://upload.wikimedia.org/wikipedia/commons/7/78/Swift_logo.png")`}}></section>
+                    <section className="magic-wall_item lazyloaded" style={{backgroundImage: `url("https://jacekjeznach.com/wp-content/uploads/2017/10/3.jpg"")`}}></section>
                     <div className="hover-content vcenter"></div>
                     <a href="/" className="colorbox cboxElement"></a>
                 </li>
                 <li id="litest4">
-                    <section className="magic-wall_item lazyloaded" style={{backgroundImage: `url("https://zupimages.net/up/21/17/obbb.png")`}}></section>
+                    <section className="magic-wall_item lazyloaded" style={{backgroundImage: `url("https://jacekjeznach.com/wp-content/uploads/2019/01/Adrian-Flux-Motorsport-e1546707189414.jpg")`}}></section>
                     <div className="hover-content vcenter"></div>
                     <a href="/" className="colorbox cboxElement"></a>
                 </li>
                 <li id="litest5">
-                    <section className="magic-wall_item lazyloaded" style={{backgroundImage: `url("https://d33wubrfki0l68.cloudfront.net/21e2a22d0240a5a1088a7ca485328bec4d836c49/b2a49/img/posts/java_logo_640.jpg")`}}></section>
+                    <section className="magic-wall_item lazyloaded" style={{backgroundImage: `url("https://jacekjeznach.com/wp-content/uploads/2017/10/ttelo.png")`}}></section>
                     <div className="hover-content vcenter"></div>
                     <a href="/" className="colorbox cboxElement"></a>
                 </li>
